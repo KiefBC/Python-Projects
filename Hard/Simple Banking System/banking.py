fb33a5b483c3394fe8aeb532d362ee1e8b4709f2
KiefBC <kiefhay642@outlook.com>
2022-11-16 13:06:11 -0800

    added the Luhn Algorithm and verification

--- Hard/Simple Banking System/banking.py
+++ Hard/Simple Banking System/banking.py
@@ -2,21 +2,24 @@ import random
 
 
 class BankMachine:
-
+    """
+    Just a simple Bank Machine program made in Python.
+    Self-explanatory. User can Create an account, login to an account or View an account
+    Nothing is currently saved once the program ends.
+    """
     def __init__(self):
         self.iin = '400000'
         self.cards = []
         self.pins = []
-        self.main_menu = ("1. Create an account\n2. Log into account\n0. Exit")
-        self.account_menu = ('1. Balance\n2. Log Out\n0. Exit')
+        self.main_menu = "1. Create an account\n2. Log into account\n0. Exit"
+        self.account_menu = '1. Balance\n2. Log Out\n0. Exit'
 
     def menu(self):
         """
         The main menu everyone will see upon accessing our Simple Banking System
         They have 3 choices. Create, Log in or Exit
+        :return:
         """
-        print(self.cards)
-        print(self.pins)
         user_input = input(f'{self.main_menu}\n> ')
         match user_input:
             case '1':
@@ -32,19 +35,26 @@ class BankMachine:
     def card_creation(self):
         """
         We create our user a new Card Number and PIN
-        We hope they keep it safe.
+        We utilize the Luhn Algorithm
+        We hope the Human Centipede keep it safe.
+        :return:
         """
-        card_creation = random.randint(999999999, 9999999999)
-        ready_card = f'{self.iin}{card_creation}'
-        self.cards.append(ready_card)
-        return f'{self.iin}{card_creation}'
+        card_creation = ''.join([str(random.randint(0, 9)) for _ in range(9)])
+        card_number = f'{self.iin}{card_creation}'
+        for checksum in range(10):
+            if self.luhn_algo(card_number + str(checksum)):
+                card_number = card_number + str(checksum)
+                break
+        self.cards.append(card_number)
+        return card_number
 
     def pin_creation(self):
         """
         This will be used to create the users PINS using a random number from 999 to 9999
         PIN's should be 4 digits long at minimum
+        :return:
         """
-        pin_creation = random.randint(999, 9999)
+        pin_creation = ''.join([str(random.randint(0, 9)) for _ in range(4)])
         ready_pin = f'{pin_creation}'
         self.pins.append(ready_pin)
         return f'{ready_pin}'
@@ -53,11 +63,13 @@ class BankMachine:
         """
         This is the basic User Account that the user will see once they have successfully logged in
         after verifying themselves through check_creds()
+        :return:
         """
         account_input = input(f'{self.account_menu}\n> ')
         match account_input:
             case '1':
                 print('Balance: 0\n')
+                return self.account()
             case '2':
                 print('You have successfully logged out!')
                 return self.menu()
@@ -65,23 +77,44 @@ class BankMachine:
                 print('Bye!')
                 return
 
+    def luhn_algo(self, card_number):
+        """
+        The Luhn algorithm will detect any single-digit error, as well as almost all transpositions of adjacent
+        digits. It will not, however, detect transposition of the two-digit sequence 09 to 90 (or vice versa). It
+        will detect most of the possible twin errors (it will not detect 22 ↔ 55, 33 ↔ 66 or 44 ↔ 77) :param
+        card_number: :return:
+        """
+        card_number = [int(num) for num in card_number]
+        for i in range(len(card_number)):
+            if i % 2 == 0:
+                card_number[i] = card_number[i] * 2
+        for i in range(len(card_number)):
+            if card_number[i] > 9:
+                card_number[i] -= 9
+        if sum(card_number) % 10 == 0:
+            return True
+        else:
+            return False
+
     def check_creds(self):
         """
         This is the function for checking and verifying the credentials
-        We received from the user.
+        we received from the user as well as utilizing the Luhn Algo
+        before accepting a PIN in case of Card Number entry error
+        :return:
         """
         print("")
         card_input = input('Enter your card number:\n> ')
         pin_input = input('Enter your PIN:\n> ')
-        if card_input in self.cards:
+        if self.luhn_algo(card_input):
             if pin_input in self.pins:
                 print('You have successfully logged in!')
                 return self.account()
             else:
-                print('Wrong card number or PIN!')
+                print('Wrong card number or PIN!\n')
                 return self.menu()
         else:
-            print('Wrong card number or PIN!')
+            print('Wrong card number or PIN!\n')
             return self.menu()
 
 
