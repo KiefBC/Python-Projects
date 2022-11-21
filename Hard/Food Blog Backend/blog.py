54f227e0d62cee100f4819a7ee9b3a74d8ee587e
KiefBC <kiefhay642@outlook.com>
2022-11-20 21:16:29 -0800

    split up into 3 files. rewrote and added complexity

---
+++ Hard/Food Blog Backend/blog.py
@@ -0,0 +1,17 @@
+from blog_backend import FoodBlogDB
+
+"""
+I don't see any reason to have our "blog" part of our backend. 
+To me, it makes more sense to separate the two.
+Have the blog itself call the backend.
+"""
+
+
+def main():
+    fb = FoodBlogDB()
+    fb.initialize_db()
+    fb.pop_table()
+
+
+if __name__ == '__main__':
+    main()
--- Hard/Food Blog Backend/blog_backend.py
+++ Hard/Food Blog Backend/blog_backend.py
@@ -2,7 +2,7 @@ import sqlite3
 import json
 
 
-class FoodBlog:
+class FoodBlogDB:
 
     def __init__(self):
         self.conn = sqlite3.connect('food_blog.db')
@@ -36,27 +36,21 @@ class FoodBlog:
                 self.conn.commit()
 
     def pop_table(self):
+        """
+        This will allow the user to add a recipe name and description to the database
+        :return:
+        """
         while True:
             recipe_name = input('Recipe name: ')
             if recipe_name == '':
                 break
 
             else:
-                self.c.execute(f"INSERT OR IGNORE INTO recipes (recipe_name) VALUES ('{recipe_name}')")
-                self.conn.commit()
                 recipe_description = input('Recipe description: ')
-                self.c.execute(f"UPDATE recipes SET recipe_description = '{recipe_description}' WHERE recipe_name = '{recipe_name}'")
+                self.c.execute(f"INSERT OR IGNORE INTO recipes (recipe_name, recipe_description) VALUES ('{recipe_name}', '{recipe_description}')")
                 self.conn.commit()
-                self.c.execute("SELECT * FROM recipes")
-                items = self.c.fetchall()
-                print(items)
                 continue
 
         # TERMINATION
         self.conn.commit()
         self.conn.close()
-
-
-if __name__ == '__main__':
-    fb = FoodBlog()
-    fb.pop_table()
