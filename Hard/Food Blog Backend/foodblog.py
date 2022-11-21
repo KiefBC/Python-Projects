84d80456468ff938c0f9ae4f5a6c9e0a2e737e61
KiefBC <kiefhay642@outlook.com>
2022-11-20 19:26:49 -0800

    rewrote the script

--- Hard/Food Blog Backend/foodblog.py
+++ Hard/Food Blog Backend/foodblog.py
@@ -4,61 +4,28 @@ import sqlite3
 conn = sqlite3.connect('food_blog.db')
 c = conn.cursor()
 
-# Create our meals table.
-c.execute("""CREATE TABLE IF NOT EXISTS meals (
-            meal_id INTEGER PRIMARY KEY,
-            meal_name TEXT UNIQUE NOT NULL)""")
+# OUR DATA
+data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
+        "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
+        "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
+
+# CREATE OUR TABLES
+c.executescript("""
+    CREATE TABLE IF NOT EXISTS meals (meal_id INTEGER PRIMARY KEY, meal_name TEXT UNIQUE NOT NULL);
+    CREATE TABLE IF NOT EXISTS ingredients (ingredient_id INTEGER PRIMARY KEY, ingredient_name TEXT UNIQUE NOT NULL);
+    CREATE TABLE IF NOT EXISTS measures (measure_id INTEGER PRIMARY KEY, measure_name TEXT UNIQUE);""")
 conn.commit()
 
-# Add items to our meals table.
-c.execute("""INSERT OR IGNORE INTO meals (meal_name)
-          VALUES
-          ("breakfast"),
-          ("brunch"),
-          ("lunch"),
-          ("supper")""")
-conn.commit()
+# ADD DATA TO OUR DB
+for i in data.keys():
+    # for loop grabs table names via i for the SELECT statement and the data[index]
+    # x += itself to create an index
+    c.executemany(f"INSERT OR IGNORE INTO {i} VALUES (?, ?)", [x for x in enumerate(data[i])])
 
 
-# Create our ingredients table.
-c.execute("""CREATE TABLE IF NOT EXISTS ingredients (
-            ingredient_id INTEGER PRIMARY KEY,
-            ingredient_name TEXT UNIQUE NOT NULL)""")
-conn.commit()
-
-# Add ingredients to our ingredients table
-c.execute("""INSERT OR IGNORE INTO ingredients (ingredient_name)
-          VALUES
-          ("milk"),
-          ("cacao"),
-          ("strawberry"),
-          ("blueberry"),
-          ("blackberry"),
-          ("sugar")""")
-conn.commit()
-
-# Create our measures table.
-c.execute("""CREATE TABLE IF NOT EXISTS measures (
-            measure_id INTEGER PRIMARY KEY,
-            measure_name TEXT UNIQUE)""")
-conn.commit()
-
-c.execute("""INSERT OR IGNORE INTO measures (measure_name)
-          VALUES
-          ("ml"),
-          ("g"),
-          ("l"),
-          ("cup"),
-          ("tbsp"),
-          ("tsp"),
-          ("dsp"),
-          ("")""")
-conn.commit()
-
-# # Verify we added it
-# c.execute("SELECT * FROM measures")
-# items = c.fetchall()
-# print(items)
+c.execute("SELECT * FROM meals")
+print(c.fetchall())
 
 # Terminate our DB Connection
+conn.commit()
 conn.close()
