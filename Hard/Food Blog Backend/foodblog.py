cf917104a7e61dab65562a6bf83885c971bdd9b1
KiefBC <kiefhay642@outlook.com>
2022-11-20 20:43:31 -0800

    added complexity

--- Hard/Food Blog Backend/foodblog.py
+++ Hard/Food Blog Backend/foodblog.py
@@ -22,7 +22,9 @@ class FoodBlog:
         self.c.executescript("""
             CREATE TABLE IF NOT EXISTS meals (meal_id INTEGER PRIMARY KEY, meal_name TEXT UNIQUE NOT NULL);
             CREATE TABLE IF NOT EXISTS ingredients (ingredient_id INTEGER PRIMARY KEY, ingredient_name TEXT UNIQUE NOT NULL);
-            CREATE TABLE IF NOT EXISTS measures (measure_id INTEGER PRIMARY KEY, measure_name TEXT UNIQUE);""")
+            CREATE TABLE IF NOT EXISTS measures (measure_id INTEGER PRIMARY KEY, measure_name TEXT UNIQUE);
+            CREATE TABLE IF NOT EXISTS recipes (recipe_id INTEGER PRIMARY KEY, recipe_name TEXT NOT NULL, recipe_description TEXT);
+                            """)
         self.conn.commit()
 
         # ADD DATA TO OUR DB
@@ -31,7 +33,24 @@ class FoodBlog:
                 # we use {item}[:-1] to remove the "s"  off the table names (notice meals and meal_id)
                 # INSERT INTO meals (meal_name) VALUES (1, "breakfast") is an example of below
                 self.c.execute(f"INSERT OR IGNORE INTO {item} ({item[:-1]}_id, {item[:-1]}_name) values({k}, '{v}');")
-        self.conn.commit()
+                self.conn.commit()
+
+    def pop_table(self):
+        while True:
+            recipe_name = input('Recipe name: ')
+            if recipe_name == '':
+                break
+
+            else:
+                self.c.execute(f"INSERT OR IGNORE INTO recipes (recipe_name) VALUES ('{recipe_name}')")
+                self.conn.commit()
+                recipe_description = input('Recipe description: ')
+                self.c.execute(f"UPDATE recipes SET recipe_description = '{recipe_description}' WHERE recipe_name = '{recipe_name}'")
+                self.conn.commit()
+                self.c.execute("SELECT * FROM recipes")
+                items = self.c.fetchall()
+                print(items)
+                continue
 
         # TERMINATION
         self.conn.commit()
@@ -40,3 +59,4 @@ class FoodBlog:
 
 if __name__ == '__main__':
     fb = FoodBlog()
+    fb.pop_table()
