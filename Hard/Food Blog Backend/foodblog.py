329d69e022d0789fdbcd7fec359d1da15c3d8f95
KiefBC <kiefhay642@outlook.com>
2022-11-20 19:35:23 -0800

    rewrote again but into a Class

--- Hard/Food Blog Backend/foodblog.py
+++ Hard/Food Blog Backend/foodblog.py
@@ -1,31 +1,36 @@
 import sqlite3
 
-# Create our DB food_blog.db
-conn = sqlite3.connect('food_blog.db')
-c = conn.cursor()
 
-# OUR DATA
-data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
-        "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
-        "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
+class FoodBlog:
 
-# CREATE OUR TABLES
-c.executescript("""
-    CREATE TABLE IF NOT EXISTS meals (meal_id INTEGER PRIMARY KEY, meal_name TEXT UNIQUE NOT NULL);
-    CREATE TABLE IF NOT EXISTS ingredients (ingredient_id INTEGER PRIMARY KEY, ingredient_name TEXT UNIQUE NOT NULL);
-    CREATE TABLE IF NOT EXISTS measures (measure_id INTEGER PRIMARY KEY, measure_name TEXT UNIQUE);""")
-conn.commit()
+    def __init__(self):
+        self.conn = sqlite3.connect('food_blog.db')
+        self.c = self.conn.cursor()
+        self.initialize_db()
 
-# ADD DATA TO OUR DB
-for i in data.keys():
-    # for loop grabs table names via i for the SELECT statement and the data[index]
-    # x += itself to create an index
-    c.executemany(f"INSERT OR IGNORE INTO {i} VALUES (?, ?)", [x for x in enumerate(data[i])])
+    def initialize_db(self):
+        # OUR DATA
+        data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
+                "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
+                "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
+
+        # CREATE OUR TABLES
+        self.c.executescript("""
+            CREATE TABLE IF NOT EXISTS meals (meal_id INTEGER PRIMARY KEY, meal_name TEXT UNIQUE NOT NULL);
+            CREATE TABLE IF NOT EXISTS ingredients (ingredient_id INTEGER PRIMARY KEY, ingredient_name TEXT UNIQUE NOT NULL);
+            CREATE TABLE IF NOT EXISTS measures (measure_id INTEGER PRIMARY KEY, measure_name TEXT UNIQUE);""")
+        self.conn.commit()
+
+        # ADD DATA TO OUR DB
+        for i in data.keys():
+            # for loop grabs table names via i for the SELECT statement and the data[index]
+            # x += itself to create an index
+            self.c.executemany(f"INSERT OR IGNORE INTO {i} VALUES (?, ?)", [x for x in enumerate(data[i])])
+            self.conn.commit()
+
+        # Terminate our DB Connection
+        self.conn.commit()
+        self.conn.close()
 
 
-c.execute("SELECT * FROM meals")
-print(c.fetchall())
-
-# Terminate our DB Connection
-conn.commit()
-conn.close()
+fb = FoodBlog()
