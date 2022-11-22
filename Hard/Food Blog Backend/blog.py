42d50a7da94cca24c17e89515bb697fbae2b9907
KiefBC <kiefhay642@outlook.com>
2022-11-21 22:42:13 -0800

    sector 5 complete but not sure how

--- Hard/Food Blog Backend/blog.py
+++ Hard/Food Blog Backend/blog.py
@@ -1,4 +1,6 @@
 from blog_backend import FoodBlogDB
+from sys import argv
+import argparse
 
 """
 I don't see any reason to have our "blog" part of our backend. 
@@ -8,9 +10,25 @@ Have the blog itself call the backend.
 
 
 def main():
-    fb = FoodBlogDB()
-    fb.pop_table()
+
+    # LETS NAME OUR DATABASE OR CHECK THE COMMANDLINE
+    db_name = argv[1] if len(argv) >= 2 else 'food_blog.db'
+    with open("moms_recipe.json") as recipe:
+        the_recipe = json.load(recipe)
+    fb = FoodBlogDB(db_name)
+
+    if len(argv) == 4:
+        ingredients = tuple(argv[2].split('=')[1].strip('"').split(','))
+        meals = tuple(argv[3].split('=')[1].strip('"').split(','))
+        print(ingredients, meals)
+        fb.recipe_query(ingredients, meals)
+    else:
+        fb.initialize_db()
+        fb.pop_table()
+
+
 
 
 if __name__ == '__main__':
+    args = arguments()
     main()
--- Hard/Food Blog Backend/blog_backend.py
+++ Hard/Food Blog Backend/blog_backend.py
@@ -1,184 +1,154 @@
 import sqlite3
-import json
+import argparse
 
 
-class FoodBlogDB:
-    """
-    Our fancy smancy, triple bling ring a ding ding Food blog DATABASE!
-    OMGOSH SO COOL
-    """
+def arguments():
+    parser = argparse.ArgumentParser(description="Accepts database name and optional values")
+    parser.add_argument("db_name", help="Database name")
+    parser.add_argument("--ingredients", type=str, help="List of ingredients split by ','")
+    parser.add_argument("--meals", type=str, help="List of meals split by ','")
+    return parser.parse_args()
 
-    def __init__(self):
-        self.conn = sqlite3.connect('food_blog.db')
-        self.c = self.conn.cursor()
-        self.initialize_db()
 
-    def initialize_db(self):
-        """
-        HAVE YOU EVER WONDERED HOW BUTTERFLIES WORK?
-        :return:
-        """
+class FoodBlog:
 
-        # TURNING ON PK AND FK
-        self.execute_query("PRAGMA foreign_keys = ON;")
+    def __init__(self, database, ingredients, meals):
+        self.data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
+                     "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
+                     "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
+        self.conn = sqlite3.connect(database)
+        self.cur = self.conn.cursor()
+        self.ingredients = ingredients
+        self.meals = meals
+        self.create_tables()
 
-        # OUR DATA
-        with open("moms_recipe.json") as recipe:
-            the_recipe = json.load(recipe)
+    def create_tables(self):
+        if len(self.cur.execute('''SELECT name FROM sqlite_master WHERE type='table';''').fetchall()):
+            return
+        self.cur.execute("PRAGMA foreign_keys = ON")
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS meals (
+                         meal_id INTEGER PRIMARY KEY,
+                         meal_name TEXT UNIQUE NOT NULL);
+                         ''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS ingredients (
+                         ingredient_id INTEGER PRIMARY KEY,
+                         ingredient_name TEXT UNIQUE NOT NULL);
+                         ''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS measures (
+                         measure_id INTEGER PRIMARY KEY,
+                         measure_name TEXT UNIQUE);
+                         ''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS recipes (
+                         recipe_id INTEGER PRIMARY KEY,
+                         recipe_name TEXT NOT NULL,
+                         recipe_description TEXT);
+                         ''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS serve (
+                         serve_id INTEGER PRIMARY KEY,
+                         recipe_id INTEGER NOT NULL,
+                         meal_id INTEGER NOT NULL,
+                         FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id),
+                         FOREIGN KEY(meal_id) REFERENCES meals(meal_id));
+                         ''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS quantity (
+                         quantity_id INTEGER PRIMARY KEY,
+                         measure_id INTEGER NOT NULL,
+                         ingredient_id INTEGER NOT NULL,
+                         quantity INTEGER NOT NULL,
+                         recipe_id INTEGER NOT NULL,
+                         FOREIGN KEY (measure_id) REFERENCES measures(measure_id),
+                         FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id),
+                         FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id));
+                         ''')
 
-        # CREATE OUR TABLES
-        self.c.executescript("""
-            CREATE TABLE IF NOT EXISTS meals (
-            meal_id INTEGER PRIMARY KEY, 
-            meal_name TEXT UNIQUE NOT NULL);
+        for table in self.data.keys():
+            for n in range(len(self.data[table])):
+                self.cur.execute(f"INSERT INTO {table} VALUES ({n + 1}, '{self.data[table][n]}')")
 
-            CREATE TABLE IF NOT EXISTS ingredients (
-            ingredient_id INTEGER PRIMARY KEY, 
-            ingredient_name TEXT UNIQUE NOT NULL);
-
-            CREATE TABLE IF NOT EXISTS measures (
-            measure_id INTEGER PRIMARY KEY, 
-            measure_name TEXT UNIQUE);
-
-            CREATE TABLE IF NOT EXISTS recipes (
-            recipe_id INTEGER PRIMARY KEY, 
-            recipe_name TEXT NOT NULL, 
-            recipe_description TEXT);
-
-            CREATE TABLE IF NOT EXISTS serve (
-            serve_id INTEGER PRIMARY KEY, 
-            recipe_id INTEGER NOT NULL, 
-            meal_id INTEGER NOT NULL,
-            FOREIGN KEY (recipe_id)
-                REFERENCES recipes (recipe_id),
-            FOREIGN KEY (meal_id)
-                REFERENCES meals (meal_id));
-
-            CREATE TABLE IF NOT EXISTS quantity (
-            quantity_id INTEGER PRIMARY KEY, 
-            measure_id INTEGER NOT NULL, 
-            ingredient_id INTEGER NOT NULL,
-            quantity INTEGER NOT NULL,
-            recipe_id INTEGER NOT NULL,
-            FOREIGN KEY (measure_id)
-                REFERENCES measures (measure_id),
-            FOREIGN KEY (ingredient_id)
-                REFERENCES ingredients (ingredient_id),
-            FOREIGN KEY (recipe_id)
-                REFERENCES recipes (recipe_id)
-            );
-            """)
         self.conn.commit()
 
-        # ADD DATA TO OUR DB
-        for item in the_recipe:
-            for k, v in enumerate(the_recipe.get(item)):
-                # we use {item}[:-1] to remove the "s"  off the table names (notice meals and meal_id)
-                # INSERT INTO meals (meal_name) VALUES (1, "breakfast") is an example of below
-                insert_query = f"INSERT OR IGNORE INTO {item} ({item[:-1]}_id, {item[:-1]}_name) values({k}, '{v}');"
-                self.execute_query(insert_query)
-
-    def verify_ingredients(self, ingredient):
-        """
-        Checking to see if the ingredient the user wants to use is in our Pantry (database)
-        Hopefully the resident mouse ignores the cheese this time....
-        :param ingredient: 
-        :return: 
-        """
-
-        rows = self.execute_query(f'SELECT ingredient_id from ingredients '
-                                  f'WHERE ingredient_name LIKE "%{ingredient}%"').fetchall()
-        if len(rows) == 1:
-            return rows[0][0]
-        return None
-
-    def verify_measure(self, measure):
-        """
-        It is inconceivable that someone could carelessly input wrong measurements!
-        Why, it would almost be a...... USER ERROR! THE HORROR!
-        :param measure: 
-        :return: 
-        """
-        if measure == "":
-            return self.execute_query('SELECT measure_id from measures '
-                                      'WHERE measure_name = ""').fetchone()[0]
-        rows = self.execute_query(f'SELECT measure_id from measures '
-                                  f'WHERE measure_name LIKE "{measure}%"').fetchall()
-        if len(rows) == 1:
-            return int(rows[0][0])
-        return None
-
-    def verify_amount(self, amount):
-        """
-        I've asked people for a cola of cola, and they look at me like I'm weird??
-        Everyone knows a cola is 1L and a cola of a cola is wanting 1L of cola...
-        :param amount: 
-        :return: 
-        """
-        if amount == "":
-            return self.execute_query('SELECT measure_id from measures '
-                                      'WHERE measure_name = ""').fetchone()[0]
-        rows = self.execute_query(f'SELECT measure_id from measures '
-                                  f'WHERE measure_name LIKE "{amount}%"').fetchall()
-        if len(rows) == 1:
-            return int(rows[0][0])
-        return None
-
-    def execute_query(self, query):
-        """
-        So much easier just being able to call a function instead of constantly
-        writing our execute commands.
-        :param query:
-        :param command:
-        :return:
-        """
-        conn = sqlite3.connect('food_blog.db')
-        c = conn.cursor()
-        result = c.execute(query)
-        conn.commit()
-        return result
-
-    def pop_table(self):
-        """
-        This will allow the user to add a recipe name and description to the database
-        :return:
-        """
-        default = 'Overcooked and dried Turkey is the worst'
-        default_ = '1 2 3'
-
+    def add_recipe(self):
         while True:
-            print('\nPass the empty recipe name to exit.\n')
-            recipe_name = input('Your Recipe Name <Press "Enter" to stop>: ')
-            if recipe_name == '':
+            print("Pass the empty recipe name to exit.")
+            new_name = input("Recipe name: ")
+            if new_name == "":
                 break
             else:
-                recipe_description = input('Describe Your Recipe: ') or default
-                insert_query = f"INSERT OR IGNORE INTO recipes (recipe_name, recipe_description) VALUES ('{recipe_name}', '{recipe_description}')"
-                recipe_id = self.execute_query(insert_query).lastrowid
-                recipe_served = input('\n1) breakfast  2) brunch  3) lunch  4) supper\nEnter proposed meals separated by a space: ').split() or default_
-                for time in recipe_served:
-                    insert_query = f'INSERT INTO serve (recipe_id, meal_id) VALUES ("{recipe_id}", "{time}")'
-                    self.execute_query(insert_query)
-                while True:
-                    quantity_input = [x for x in input('\nInput quantity of ingredient <Press "Enter" to stop>: ').split()]
-                    if len(quantity_input) != 0:
-                        the_amount, the_ingredient = [quantity_input[1], quantity_input[-1]]
-                        the_measure = "" if len(quantity_input) == 2 else quantity_input[1]
-
-                        # VERIFY THE DATA
-                        quantity = self.verify_amount(the_amount)
-                        measure_id = self.verify_measure(the_measure)
-                        ingredient_id = self.verify_ingredients(the_ingredient)
-
-                        # If something is None .... BAD!
-                        if None in [quantity, measure_id, ingredient_id]:
-                            print('\nRecipes is inconclusive, please try again!')
-                        else:
-                            print('\nInserting data into the database....')
-                            insert_query = f'INSERT INTO quantity (measure_id, ingredient_id, quantity, recipe_id) VALUES ({measure_id}, {ingredient_id}, {quantity}, {recipe_id})'
-                            self.execute_query(insert_query)
-                            print('\nData input complete. Thanks for using OBSTERGO. Enjoy your soy.')
-                    else:
-                        break
-        # TERMINATION ###
+                new_description = input("Recipe description: ")
+            last_row = self.cur.execute(f"INSERT INTO recipes (recipe_name, recipe_description) "
+                                        f"VALUES ('{new_name}', '{new_description}');").lastrowid
+            self.proposed_meals(last_row)
+            self.quantity_of_ingredient(last_row)
+            self.conn.commit()
         self.conn.close()
+
+    def proposed_meals(self, recipe):
+        print("1) breakfast  2) brunch  3) lunch  4) supper")
+        new_meals = [int(i) for i in input("When the dish can be served: ").split(" ")]
+        for meal in new_meals:
+            self.cur.execute(f"INSERT INTO serve (recipe_id, meal_id)"
+                             f"VALUES ({recipe}, {meal});")
+
+    def quantity_of_ingredient(self, recipe):
+        while True:
+            values = input("Input quantity of ingredients <press enter to stop>: ")
+            if values == "":
+                break
+            else:
+                values = [x for x in values.split(" ")]
+            if len(values) == 2:
+                values.insert(1, "")
+                measure_check = 1
+            else:
+                measure_check = self.cur.execute(f"SELECT COUNT(*) FROM measures "
+                                                 f"WHERE measure_name LIKE '%{values[1]}%';").fetchone()[0]
+            ingredient_check = self.cur.execute(f"SELECT COUNT(*) FROM ingredients "
+                                                f"WHERE ingredient_name LIKE '%{values[2]}%';").fetchone()[0]
+
+            if measure_check != 1 or ingredient_check != 1:
+                print("The ingredient is not conclusive!")
+            else:
+                m_id = self.cur.execute(f'''SELECT measure_id FROM measures 
+                                        WHERE measure_name LIKE '%{values[1]}%';''').fetchone()[0]
+                i_id = self.cur.execute(f'''SELECT ingredient_id FROM ingredients 
+                                        WHERE ingredient_name LIKE '%{values[2]}%';''').fetchone()[0]
+                self.cur.execute(f'''INSERT INTO quantity (quantity, recipe_id, measure_id, ingredient_id) 
+                                 VALUES ({values[0]}, {recipe}, {m_id}, {i_id});''')
+
+    def find_recipes(self):
+        ingredients, meals = self.ingredients.split(','), self.meals.split(',')
+        self.cur.execute(
+            '''CREATE VIEW IF NOT EXISTS all_tables AS 
+            SELECT * FROM meals AS m
+            JOIN serve AS s ON m.meal_id=s.meal_id
+            JOIN recipes AS r ON s.recipe_id=r.recipe_id
+            JOIN quantity AS q ON r.recipe_id=q.recipe_id
+            JOIN ingredients AS i ON q.ingredient_id=i.ingredient_id;'''
+        )
+
+        start_query = """SELECT recipe_name FROM all_tables AS a 
+        WHERE ingredient_name = '{}'""".format(ingredients[0])
+
+        exists_query = """AND EXISTS (SELECT recipe_name FROM all_tables 
+        WHERE ingredient_name = '{}' AND recipe_name = a.recipe_name)"""
+
+        meals = "', '".join(meals)
+        ingredients = " ".join([exists_query.format(x) for x in ingredients[1:]])
+        full_query = f"""{start_query} {ingredients} AND meal_name in ('{meals}') ORDER BY recipe_name;"""
+        recipes = self.cur.execute(full_query).fetchall()
+        if len(recipes) > 0:
+            suitable_recipes = ", ".join([x[0] for x in recipes])
+            print(f'Recipes selected for you: {suitable_recipes}')
+        else:
+            print('There are no such recipes in the database.')
+
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    args = arguments()
+    food_blog_backend = FoodBlog(args.db_name, args.ingredients, args.meals)
+    if food_blog_backend.ingredients is None:
+        food_blog_backend.add_recipe()
+    else:
+        food_blog_backend.find_recipes()
