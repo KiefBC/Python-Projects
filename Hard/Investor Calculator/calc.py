41fda5cf6085ef66f4e9585db8088ea193928ecd
KiefBC <kiefhay642@outlook.com>
2022-12-03 17:24:19 -0800

    initial commit

--- Hard/Investor Calculator/calc.py
+++ Hard/Investor Calculator/calc.py
@@ -1,20 +1,19 @@
-# Will we need these forever? Who knows
+import csv
+import database
+
 MAIN_MENU = "MAIN MENU\n0 Exit\n1 CRUD operations\n2 Show top ten companies by criteria"
 CRUD_MENU = "CRUD MENU\n0 Back\n1 Create a company\n2 Read a company\n3 Update a company\n4 Delete a company\n5 List " \
             "all companies"
 TOP_TEN_MENU = "TOP TEN MENU\n0 Back\n1 List by ND/EBITDA\n2 List by ROE\n3 List by ROA"
 NICE_DAY = "Have a nice day!"
-BAD_DAY = "Not implemented!"
 
 
 class InvestCalc:
-
-    def __init__(self):
-        pass
+    pass
 
 
 def main():
-    calc = InvestCalc()
+    ic = InvestCalc()
     while True:
         menu_input = input(f'{MAIN_MENU}\nEnter an option: ')
         match menu_input:
@@ -25,7 +24,7 @@ def main():
                         print(NICE_DAY)
                         exit()
                     case _:
-                        print(BAD_DAY)
+                        print('Not implemented!')
             case '2':
                 ten_menu = input(f'{TOP_TEN_MENU}\nEnter an option: ')
                 match ten_menu:
@@ -33,7 +32,7 @@ def main():
                         print(NICE_DAY)
                         exit()
                     case _:
-                        print(BAD_DAY)
+                        print('Not implemented!')
             case '0':
                 print(NICE_DAY)
                 exit()
@@ -42,4 +41,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    database.main()
---
+++ Hard/Investor Calculator/database.py
@@ -0,0 +1,78 @@
+import csv
+
+from sqlalchemy import create_engine, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///investor.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class CompanyDatabase(Base):
+    __tablename__ = 'Companies'
+
+    ticker = Column(String, primary_key=True)
+    name = Column(String)
+    sector = Column(String)
+
+
+class FinancialDatabase(Base):
+    __tablename__ = 'Financials'
+
+    ticker = Column(String, primary_key=True)
+    ebitda = Column(Float)
+    sales = Column(Float)
+    net_profit = Column(Float)
+    market_price = Column(Float)
+    net_debt = Column(Float)
+    assets = Column(Float)
+    equity = Column(Float)
+    cash_equivalents = Column(Float)
+    liabilities = Column(Float)
+
+
+def main():
+    Base.metadata.create_all(engine)
+
+    with open("companies.csv", newline='') as companies:
+        file_reader = csv.reader(companies, delimiter=",")  # Create a reader object
+        for line in file_reader:  # Read each line
+            ticker = line[0]
+            name = line[1]
+            sector = line[2]
+            add_company = CompanyDatabase(ticker=ticker, name=name, sector=sector)
+            session.add(add_company)
+
+        with open("financial.csv", newline='') as financials:
+            columns = ['Ticker', 'EBITDA', 'Sales', 'Net Profit', 'Market Price', 'Net Debt',
+                       'Assets', 'Equity', 'Cash Equivalents', 'Liabilities']
+            file_reader = csv.DictReader(financials, delimiter=",")
+            for line in file_reader:
+                for v in line.items():
+                    if v[1] == '':
+                        line[v[0]] = None
+                ticker = line['ticker']
+                ebitda = line['ebitda']
+                sales = line['sales']
+                net_profit = line['net_profit']
+                market_price = line['market_price']
+                net_debt = line['net_debt']
+                assets = line['assets']
+                equity = line['equity']
+                cash_equivalents = line['cash_equivalents']
+                liabilities = line['liabilities']
+                add_financial = FinancialDatabase(ticker=ticker, ebitda=ebitda, sales=sales, net_profit=net_profit,
+                                                  market_price=market_price, net_debt=net_debt, assets=assets,
+                                                  equity=equity, cash_equivalents=cash_equivalents,
+                                                  liabilities=liabilities)
+                session.add(add_financial)
+
+    session.commit()
+    print('Database created successfully!')
+
+
+if __name__ == '__main__':
+    main()
